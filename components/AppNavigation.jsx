fb771350a6297db88ecc2a93a20bd798e49f776d
Mohamed Nidal <touhami9@gmail.com>
2023-09-28 15:31:29 +0100

    refactor the package

--- components/AppNavigation.jsx
+++ components/AppNavigation.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Onboarding from '../screens/Onboarding';
 
 const Stack = createNativeStackNavigator();
 
-const AppNavigation = () => {
+function AppNavigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Onboarding">
@@ -18,8 +17,6 @@ const AppNavigation = () => {
       </Stack.Navigator>
     </NavigationContainer>
   );
-};
+}
 
 export default AppNavigation;
-
-const styles = StyleSheet.create({});
